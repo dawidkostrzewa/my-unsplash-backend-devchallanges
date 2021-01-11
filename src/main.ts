ab7641f81b3dbb15b33a2240b7043c946d243e65
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2021-01-11 22:49:23 +0100

    Connect with cloudinary API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.C_API_KEY,
+  api_secret: process.env.C_API_SECRET,
+});
 
 const app = express();
 const port = 8000;
 
-app.get('/', (_req, res) => {
-  res.status(404).json('Hello World!');
+app.get('/ok', (_req, res) => {
+  res.json('Hello World!');
+});
+
+app.get('/upload', (_req, res) => {
+  cloudinary.uploader.upload('pizza2.jpeg', function (error, result) {
+    console.log(result, error);
+  });
+  res.json('ok');
 });
 
 app.listen(port, () => {
